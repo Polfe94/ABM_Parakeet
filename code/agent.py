00991a09c5e49fbb751edae484dcc9707cee4a9f
pfernandez@ceab.csic.es <pfernandez@ceab.csic.es>
2022-04-01 11:45:41 +0200

    Update

--- code/agent.py
+++ code/agent.py
@@ -12,98 +12,99 @@ STATES AND TAGS
 '''
 
 class State():   
-	
-	REPRODUCING = 'R'
-	COLONIZING = 'C'
-	STAYING = 'S'
-	GROWING = 'G'
+    
+    REPRODUCING = 'R'
+    COLONIZING = 'C'
+    STAYING = 'S'
+    GROWING = 'G'
 
 class Tag():
 
-	ADULT = 1 # More than 3 years
-	YOUNG = 0 # Less or equal than 3 years
+    ADULT = 1 # More than 3 years
+    YOUNG = 0 # Less or equal than 3 years
 
 '''
 PARAKEET AGENT 
 '''
 
 class Parakeet():
-	
-	def __init__(self, unique_id, dispersal = 'jump'):
+    
+    def __init__(self, unique_id, dispersal = 'jump'):
 
-		# id initialization
-		self.id = unique_id
-		
-		# Position, state and rates initialization
-		self.pos = params.start_node
-		self.state = 'S'
-		self.prev_state = 'S'
-		self.state_history = []
-		self.r_i = 0
+        # id initialization
+        self.id = unique_id
+        
+        # Position, state and rates initialization
+        self.pos = params.start_node
+        self.state = 'S'
+        self.prev_state = 'S'
+        self.state_history = []
+        self.r_i = 0
   
-  		# Dispersal movement
-		self.move = self.choose_dispersal_strategy(dispersal)
+          # Dispersal movement
+        self.move = self.choose_dispersal_strategy(dispersal)
 
-		# Age tag
-		self.age = -1 # new borns
-		self.tag = Tag.YOUNG ## Tag.ADULT ? ## int(round(random.random()))
+        # Age tag
+        self.age = -1 # new borns
+        self.tag = Tag.YOUNG ## Tag.ADULT ? ## int(round(random.random()))
 
-	# Pick the appropriate dispersal strategy for parakeets
-	def choose_dispersal_strategy(cls, dispersal):
+    # Pick the appropriate dispersal strategy for parakeets
+    def choose_dispersal_strategy(cls, dispersal):
 
-		if dispersal == 'jump':
-			return cls.dispersal_jump
+        if dispersal == 'jump':
+            return cls.dispersal_jump
 
-		elif dispersal == 'moore':
-			return cls.dispersal_moore
+        elif dispersal == 'moore':
+            return cls.dispersal_moore
 
-		elif dispersal == 'neumann':
-			return cls.dispersal_neumann
+        elif dispersal == 'neumann':
+            return cls.dispersal_neumann
 
-	def dispersal_jump(self, grid):
-		alpha = 2 * math.pi * random.random()
-		distance = random.randrange(10) ## change
+    def dispersal_jump(self, grid):
+        alpha = 2 * math.pi * random.random()
+        distance = random.randrange(10) ## change
   
-		# rotation to x axis
-		x, y = self.pos[0] * math.cos(alpha), self.pos[1] + distance
-		# compute Euclidean distances
-		closest_cell = np.linalg.norm(grid.pos - (x, y), axis=1)
-		minxy = np.where(min(closest_cell))[0]
-		self.pos = grid.pos[int(minxy)]
-	
-	def dispersal_moore(self, grid):
-		pass
+        # rotation to x axis
+        x, y = self.pos[0] * math.cos(alpha), self.pos[1] + distance
+        # compute Euclidean distances
+        k = list(grid.coords.keys())
+        closest_cell = np.linalg.norm(np.array(k) - np.array([(x, y)]), axis=1)
+        minxy = np.where(closest_cell == min(closest_cell))[0]
+        self.pos = k[int(minxy)]
+    
+    def dispersal_moore(self, grid):
+        pass
 
-	def dispersal_neumann(self, grid):
-	 	pass
-	
+    def dispersal_neumann(self, grid):
+         pass
+    
  
-	def die(self):
-		 pass
+    def die(self):
+         pass
+    
+    def mate(self):
+        pass
 
-	def mate(self):
-		pass
+    def grow(self):
+        self.age += 1
+        if self.age == params.adulthood:
+            self.tag = Tag.ADULT
 
-	def grow(self):
-		self.age += 1
-		if self.age == params.adulthood:
-			self.tag = Tag.ADULT
-
-	def stay(self):
-		pass
+    def stay(self):
+        pass
 
 
 
-	# Possible actions the parakeet may take, based on the position and the state
-	# returns true if food is found and picked up
-	# this value is fed into the gillespie algorithm
-	def action(self):
+    # Possible actions the parakeet may take, based on the position and the state
+    # returns true if food is found and picked up
+    # this value is fed into the gillespie algorithm
+    def action(self):
      
-		self.grow()
-	 
-		if self.tag == Tag.YOUNG:
-			if self.age == 0:
-				self.stay()
-		
-		else:
-			self.move()
+        self.grow()
+     
+        if self.tag == Tag.YOUNG:
+            if self.age == 0:
+                self.stay()
+        
+        else:
+            self.move()
---
+++ code/lattice.py
@@ -0,0 +1,51 @@
+# import networkx as nx
+import numpy as np
+
+class Lattice():
+
+    """A model with some number of agents."""
+
+    def __init__(self, width, height):
+
+        # Create squared lattice
+        '''
+
+        self.G = nx.grid_2d_graph(width, height)
+        self.coords = dict( (n, n) for n in self.G.nodes() )
+          '''
+
+
+        self.G = np.zeros((width, height), dtype = int)
+        
+        self.leftX = [width] + list(range(width + 1))
+        self.rightX = list(range(width + 1)) + [0]
+        self.bottomY = [height] + list(range(height + 1))
+        self.topY = list(range(height + 1)) + [0]
+        
+        # eight neighbors from top-left to mid-left
+        self.moore = [(-1, 1), (0, 1), (1, 1), (1, 0),
+                      (1, -1), (0, -1), (-1, -1), (-1, 0)]
+        
+        # four neighbors
+        self.neumann = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+  
+        '''
+        VISUALIZATION
+        import matplotlib.pyplot as plt
+        labels = dict( ((i, j), i * 10 + j) for i, j in self.G.nodes() )
+        nx.draw_networkx(self.G, pos=self.coords, labels=labels)
+
+        plt.axis('off')
+        plt.show()
+        '''
+        
+    def get_neighbors_moore(self, pos):
+        n = []
+        for i in self.moore:
+            pass
+            
+    # def get_neighbors(self, pos):
+    #     return list(self.G.neighbors(pos))
+
+    def mating_spots(self, coords):
+        pass
